d29637e8230b77dc5568fe3d563961490ae43a8d
Vinod Vishwanath <vishwanath.vin@husky.neu.edu>
2017-11-28 19:46:09 -0500

    Implemented ps system call

--- proc.c
+++ proc.c
@@ -532,3 +532,15 @@ procdump(void)
     cprintf("\n");
   }
 }
+
+int 
+ps() {
+  struct proc *p;
+
+  cprintf("PID\tNAME\n");
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    if(p->state == RUNNING)
+      cprintf("%d\t%s\n", p->pid, p->name);
+
+  return 0;
+}
---
+++ ps.c
@@ -0,0 +1,14 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "stat.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+main() {
+	
+	ps();
+	exit();
+	return 0;
+}
