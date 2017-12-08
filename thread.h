d84b388c1fb1991d310f770bf1b9ec88298e8a20
shraddhaphadnis <phadnis.s@husky.neu.edu>
2017-12-07 20:37:43 -0500

    Implemented locks

--- thread.h
+++ thread.h
@@ -2,9 +2,15 @@
 #ifndef THREAD_H
 #define THREAD_H
 
+struct lock_t {
+    uint lock;
+};
 int thread_create(void (*func)(), void (*arg));
 int thread_id(void);
 void thread_exit(void);
 int thread_wait(void);
+void lock_init(struct lock_t *);
+void thread_lock(struct lock_t *);
+void thread_unlock(struct lock_t *);
 
 #endif // THREAD_H
